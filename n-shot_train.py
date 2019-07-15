3ec8e8b258d5fedf50b88eecef731c73b6c91adc
akshita0208 <35263016+akshita0208@users.noreply.github.com>
2019-07-15 18:48:58 +0530

    Create n-shot_train.py

---
+++ n-shot_train.py
@@ -0,0 +1,38 @@
+
+def make_oneshot_task(N, s="val", language=None):
+    """Create pairs of test image, support set for testing N way one-shot learning. """
+    if s == 'train':
+        X = Xtrain
+        categories = train_classes
+    else:
+        X = Xval
+        categories = val_classes
+    n_classes, n_examples, w, h = X.shape
+    
+    indices = rng.randint(0, n_examples,size=(N,))
+    if language is not None: # if language is specified, select characters for that language
+        low, high = categories[language]
+        if N > high - low:
+            raise ValueError("This language ({}) has less than {} letters".format(language, N))
+        categories = rng.choice(range(low,high),size=(N,),replace=False)
+        else: # if no language specified just pick a bunch of random letters
+        categories = rng.choice(range(n_classes),size=(N,),replace=False)            
+    
+    true_category = categories[0]
+   
+
+  
+  def test_oneshot(model, N, k, s = "val", verbose = 0):
+    """Test average N way oneshot learning accuracy of a siamese neural net over k one-shot tasks"""
+    n_correct = 0
+    if verbose:
+        print("Evaluating model on {} random {} way one-shot learning tasks ... \n".format(k,N))
+    for i in range(k):
+        inputs, targets = make_oneshot_task(N,s)
+        probs = model.predict(inputs)
+        if np.argmax(probs) == np.argmax(targets):
+            n_correct+=1
+    percent_correct = (100.0 * n_correct / k)
+    if verbose:
+        print("Got an average of {}% {} way one-shot learning accuracy \n".format(percent_correct,N))
+    return percent_correct
